7d312558224eaa1d6dbdcb30feda13642e4fb2ed
DaveMorais <dmorais@ccs.usherbrooke.ca>
2017-05-24 10:39:32 -0400

    added functionalities: - workflow upload - inputs

--- bioblend/test_workflow.py
+++ bioblend/test_workflow.py
@@ -67,13 +67,13 @@ def create_history(gi, name):
 
     data = namedtuple("history", 'name id')
 
-    new_hist = gi.histories.create_history(name=name)
-    history = data(new_hist['name'], new_hist['id'])
+    hist_obj = gi.histories.create_history(name=name)
+    history = data(hist_obj['name'], hist_obj['id'])
 
     return history
 
 
-def upload_file(gi, input, hist_id, dbkey=None):
+def upload_file(gi, input, input_path, hist_id, dbkey=None):
     '''
 
     :param gi: a galaxy instance object
@@ -87,17 +87,65 @@ def upload_file(gi, input, hist_id, dbkey=None):
 
     for i in range(len(input)):
         if len(dbkey) > i:
-            file = gi.tools.upload_file(path=input[i], history_id=hist_id, dbkey=dbkey[i])
+            file_obj = gi.tools.upload_file(path=input_path + input[i], history_id=hist_id, dbkey=dbkey[i])
         else:
-            file = gi.tools.upload_file(path=input[i], history_id=hist_id)
+            file_obj = gi.tools.upload_file(path=input_path + input[i], history_id=hist_id)
 
-        d = namedtuple("dataset", 'name id')
-        data = d(file['outputs'][0]['name'], file['outputs'][0]['id'])
-
+        d = namedtuple('dataset', 'name id')
+        data = d(file_obj['outputs'][0]['name'], file_obj['outputs'][0]['id'])
         dataset.append(data)
 
-    print dataset
+    return dataset
 
+
+def get_workflow_id(gi, workflow_name, workflow_path):
+    '''
+
+    :param gi: a galaxy instance object
+    :param workflow_name: workflow name (str)
+    :param workflow_path: workflow path (str)
+    :return: a namedtuples with workflow name, id
+    '''
+
+    workflows = []
+    work_obj = gi.workflows.get_workflows()
+
+    w = namedtuple('workflow', 'name id')
+    work = ''
+    for item in work_obj:
+
+        # If workflow already exist (or has been already auploaded via API) return its name and id
+        if item['name'] == workflow_name or item['name'] + ' (imported from API)':
+            work = w(item['name'], item['id'])
+            workflows.append(work)
+
+        # call upload method in and return the workflow name and id
+        else:
+            name, w_id = _upload_workflow(gi,workflow_name, workflow_path)
+            work = w(name, w_id)
+            workflows.append(work)
+
+    return work
+
+
+def _upload_workflow(gi, workflow_name, workflow_path):
+
+    work_obj = gi.workflows.import_workflow_from_local_path(file_local_path=workflow_path + workflow_name + ".ga")
+
+    return work_obj['name'], work_obj['id']
+
+
+def workflow_inputs(gi, workflow_id):
+
+
+    work_obj = gi.workflows.show_workflow(workflow_id=workflow_id)
+
+    print work_obj['inputs']
+
+
+# wf = gi.workflows.show_workflow(workflow_id='c48e74a956218c05')
+# # pprint.pprint(wf)
+# print wf['inputs']
 
 def main():
 
@@ -117,8 +165,10 @@ def main():
 
     for workflow in workflow_exp:
 
-        history = create_history(gi, workflow.name)
-        upload_file(gi, workflow.inputs, history.id, workflow.dbkey)
+        # history = create_history(gi, workflow.name)
+        # datasets = upload_file(gi, workflow.inputs, workflow.inputs_path, history.id, workflow.dbkey)
+        g_workflow = get_workflow_id(gi, workflow.name, workflow.workflow_path)
+        inputs = workflow_inputs(gi, g_workflow.id)
 
 
 if __name__ == "__main__":
