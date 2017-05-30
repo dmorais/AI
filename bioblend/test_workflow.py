af49ff115c5db5779ac1300c66821a4683fa540a
DaveMorais <dmorais@ccs.usherbrooke.ca>
2017-05-30 14:00:27 -0400

    Documented yml

--- bioblend/test_workflow.py
+++ bioblend/test_workflow.py
@@ -163,8 +163,8 @@ def workflow_inputs(gi, workflow_id):
     w = namedtuple('inputs', 'index label')
 
     work_obj = gi.workflows.show_workflow(workflow_id=workflow_id)
-    pprint.pprint(work_obj)
-    sys.exit(0)
+    # pprint.pprint(work_obj)
+    # sys.exit(0)
 
     for k, v in work_obj['inputs'].iteritems():
         w_input = w(k, v['label'])
