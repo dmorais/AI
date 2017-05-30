2427e1f65900736e90c2be1d652a711310a1e5d6
DaveMorais <dmorais@ccs.usherbrooke.ca>
2017-05-30 11:35:00 -0400

    new func: upload from local and url

--- bioblend/create_library.py
+++ bioblend/create_library.py
@@ -85,9 +85,10 @@ def main():
         library = create_libary(gi, item.name, item.description)
         folder = create_folder(gi, library.id, item.folder)
 
-        if item.inputs:
+        if hasattr(item, 'input'):
 
             uploads = upload_from_local(gi, library.id, item.input_path, item.inputs, folder.id)
+
         else:
             uploads = upload_from_url(gi, library.id, item.urls, folder.id)
 
