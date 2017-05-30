d6154e9f4b92410c79b66f07d5a8676ba588a0c4
DaveMorais <dmorais@ccs.usherbrooke.ca>
2017-05-30 18:25:25 -0400

    debigging

--- bioblend/utils/giobjects.py
+++ bioblend/utils/giobjects.py
@@ -91,7 +91,7 @@ def get_workflow_id(gi, workflow_name, logger, workflow_path=None):
 
     else:
         for item in work_obj:
-
+            print item['name']
             # If workflow already exist (or has been already auploaded via API) return its name and id
             if item['name'] == workflow_name or item['name'] + ' (imported from API)':
                 work = w(item['name'], item['id'])
