e05322126643ac452664d82c0d33e2e43a4194b4
DaveMorais <dmorais@ccs.usherbrooke.ca>
2017-05-30 18:40:33 -0400

    debigging

--- bioblend/utils/giobjects.py
+++ bioblend/utils/giobjects.py
@@ -153,7 +153,7 @@ def create_wf_input_dict(gi, datasets, inputs, data, labels, src, logger):
     input_dict = dict()
     label_dict = dict(zip(data, labels))
 
-    print.pprint(label_dict)
+    pprint.pprint(label_dict)
     sys.exit(0)
 
     # Map each dataset name to a label
