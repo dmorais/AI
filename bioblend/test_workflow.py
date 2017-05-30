304a2267200d9afa726ef22d76f42d1ba13f50fd
DaveMorais <dmorais@ccs.usherbrooke.ca>
2017-05-30 14:11:14 -0400

    Documented yml

--- bioblend/test_workflow.py
+++ bioblend/test_workflow.py
@@ -10,6 +10,11 @@
 #    6- Get the workflow id and inputs
 #    7- builds the input dictionary
 #    8- runs the workflow
+#
+# UNFORTUNATELY a locally uploaded workflow will fail because
+# Galaxy resets the input label (or name depending on the version) back to input dataset
+# In this case open the workflow editor in Galaxy and rename the label (or name) of the inputs
+# So it matches the label in the yaml file.
 ############################################
 
 import sys
@@ -28,46 +33,6 @@ mkpath(os.getcwd() + "/logs/")
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 initialize_logger(os.getcwd() + "/logs/", logger)
-
-# def get_galaxy_instance(api_key):
-#     '''
-#     :param api_key:
-#     :return: a galaxy instance object
-#     '''
-#     with open(api_key, 'r') as api:
-#         try:
-#             url, key = api.read().strip().split(',')
-#             gi = galaxy.GalaxyInstance(url=url, key=key)
-#             return gi
-#
-#         except IOError:
-#             logger.error('Failed to open file api_key', exc_info=True)
-#             print "cannot open", api_key
-#
-#
-# def read_workflow(yaml_file):
-#     '''
-#     :param yaml_file:
-#     :return: workflow_exp: a list of named tuples (can be accessed as objects)
-#     '''
-#
-#     workflows = ''
-#     workflow_exp = []
-#     with open(yaml_file, 'r') as stream:
-#         try:
-#             workflows = yaml.load(stream)
-#
-#         except yaml.YAMLError as exc:
-#             logger.error('Failed to open file yaml file', exc_info=True)
-#             print exc
-#
-#     # Create namedtuple from dictionary
-#     for work in workflows:
-#         workflow_nametup = namedtuple("workflow", work.keys())(*work.values())
-#         workflow_exp.append(workflow_nametup)
-#
-#     return workflow_exp
-
 
 def create_history(gi, name):
     '''
@@ -163,8 +128,6 @@ def workflow_inputs(gi, workflow_id):
     w = namedtuple('inputs', 'index label')
 
     work_obj = gi.workflows.show_workflow(workflow_id=workflow_id)
-    # pprint.pprint(work_obj)
-    # sys.exit(0)
 
     for k, v in work_obj['inputs'].iteritems():
         w_input = w(k, v['label'])
