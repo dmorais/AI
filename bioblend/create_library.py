0649fc93cf5070f1c755de32a457d72d9c316056
DaveMorais <dmorais@ccs.usherbrooke.ca>
2017-05-30 10:54:37 -0400

    New script:  Create library, folder and upload file

--- bioblend/create_library.py
+++ bioblend/create_library.py
@@ -23,7 +23,7 @@ initialize_logger(os.getcwd() + "/logs/", logger)
 
 def create_libary(gi, name, description):
 
-    logger.info("Creating Library", name)
+    logger.info("Creating Library")
     lib_obj = gi.libraries.create_library(name=name, description=description)
 
     l = namedtuple('library', 'id name')
@@ -32,16 +32,16 @@ def create_libary(gi, name, description):
     return library
 
 
-# def create_folder(gi, lib_id, name):
-#
-#
-#     logger.info("Creating Folder ", name)
-#     folder_obj = gi.libraries.create_folder(library_id=lib_id, folder_name=name)
-#
-#     f = namedtuple('folder', 'id name')
-#     folder = f(folder_obj[0]['id'], folder_obj[0]['name'])
-#
-#     return folder
+def create_folder(gi, lib_id, name):
+
+
+    logger.info("Creating Folder")
+    folder_obj = gi.libraries.create_folder(library_id=lib_id, folder_name=name)
+
+    f = namedtuple('folder', 'id name')
+    folder = f(folder_obj[0]['id'], folder_obj[0]['name'])
+
+    return folder
 
 
 
@@ -62,7 +62,7 @@ def main():
     for item in yaml_file:
 
         library = create_libary(gi, item.name, item.description)
-        #folder = create_folder(gi, library.id, item.folder)
+        folder = create_folder(gi, library.id, item.folder)
 
 
     logger.info("Done")
