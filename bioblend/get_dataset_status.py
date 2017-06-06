955066383bd3408d0c5ddc6a86d11b87fed2a860
DaveMorais <dmorais@ccs.usherbrooke.ca>
2017-06-06 16:36:49 -0400

    modify get_dataset_status

--- bioblend/get_dataset_status.py
+++ bioblend/get_dataset_status.py
@@ -37,8 +37,8 @@ def get_dataset_id(gi, hist_id):
 
     d = namedtuple("dataset", 'ok id discarded id ')#failed_metadata upload paused running error new queued empty')
 
-    dataset = d('ok', d['ok'])
-                #, 'discarded', d['discarded'], 'failed_metadata', d['failed_metadata'],
+    dataset = d('ok', d['ok'], 'discarded', d['discarded'])
+                #,  'failed_metadata', d['failed_metadata'],
                #'upload', d['upload'], 'paused', d['paused'], 'running', d['running'], 'error', d['error'],
                #'new', d['new'], 'queued', d['queued'], 'empty', d['empty'])
 
